3462734bfe73b90dd7d179ff294152a2557e21b7
Dolon Roy <dolonr718@gmail.com>
2023-12-20 23:07:09 +0600

    search phone with price, name, os memory, type or processor method implement commit

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true
+  }));
 
 const uri = `mongodb+srv://${process.env.DATA_USERNAME}:${process.env.DATA_PASSWORD}@cluster0.evacz3b.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,14 +28,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    // price, name, type, processor, memory, OS
     const phoneCollection = client.db("DevTowTask").collection("mobileCollections");
-
 
     app.get("/all-phones", async (req, res) => {
         try {
+            const searchQuery = req.query.searchQuery;
+        
+            if(searchQuery) {
+               const query = {
+                    $or: [
+                            {price: {$regex: searchQuery, $options: 'i'}},
+                            {name: {$regex: searchQuery, $options: 'i'}},
+                            {os: {$regex: searchQuery, $options: 'i'}},
+                            {memory: {$regex: searchQuery, $options: 'i'}},
+                            {type: {$regex: searchQuery, $options: 'i'}},
+                            {processor: {$regex: searchQuery, $options: 'i'}}
+
+                        ]
+                };
+
+                const result = await phoneCollection.find(query).toArray();
+                return res.send(result);
+            }
 
             const result = await phoneCollection.find().toArray();
+            res.send(result);
+
+        } catch (err) {
+            console.log(err);
+        }
+    })
+
+    app.get("/phone/:id", async (req, res) => {
+        try {
+
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+
+            const result = await phoneCollection.findOne(query);
             res.send(result);
 
         } catch (err) {
